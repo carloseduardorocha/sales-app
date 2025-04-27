43f50c8f000730838bf377dc6e101055a8af87c8
Carlos Rocha <carlos.rocha548@hotmail.com>
2025-04-27 11:41:56 -0300

    refactor: updated Menubar configs after tests

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,69 @@
 <script setup>
-    import { ref, computed, onMounted, onBeforeUnmount } from 'vue';
-    import Menubar from 'primevue/menubar';
-    import { useLogo } from '@/composables/useLogo';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import Menubar from 'primevue/menubar';
+import { useLogo } from '@/composables/useLogo';
 
-    const { logo } = useLogo();
+const router = useRouter();
+const { logo } = useLogo();
 
-    const items = ref([
-        {
-            label: 'Sellers',
-            icon: 'pi pi-users',
-            to: '/sellers'
-        },
-        {
-            label: 'Sales',
-            icon: 'pi pi-shopping-cart',
-            to: '/sales'
-        },
-        {
-            label: 'Logout',
-            icon: 'pi pi-sign-out',
-            to: '/logout'
-        }
-    ]);
+const items = ref([
+    {
+        label: 'Sellers',
+        icon: 'pi pi-users',
+        route: '/sellers'
+    },
+    {
+        label: 'Sales',
+        icon: 'pi pi-shopping-cart',
+        route: '/sales'
+    },
+    {
+        label: 'Logout',
+        icon: 'pi pi-sign-out',
+        route: '/logout'
+    }
+]);
 </script>
 
 <template>
     <div class="menubar-container">
-        <img class="logo" :src="logo" alt="Logo" width="80"/>
+        <img class="logo" :src="logo" alt="Logo" width="80" />
         <Menubar :model="items" class="menubar">
-            <template #start>
-                <!-- Logo já está no início -->
+            <template #item="{ item, props, hasSubmenu }">
+                <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
+                    <a v-ripple :href="href" v-bind="props.action" @click="navigate">
+                        <span :class="item.icon" />
+                        <span>{{ item.label }}</span>
+                    </a>
+                </router-link>
+                <a v-else v-ripple :href="item.url" :target="item.target" v-bind="props.action">
+                    <span :class="item.icon" />
+                    <span>{{ item.label }}</span>
+                    <span v-if="hasSubmenu" class="pi pi-fw pi-angle-down" />
+                </a>
             </template>
         </Menubar>
     </div>
-    <router-view></router-view>
+    <router-view />
 </template>
 
 <style scoped>
-    .menubar-container {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-    }
+.menubar-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
 
-    .logo {
-        margin-right: 20px;
-    }
+.logo {
+    margin-right: 20px;
+}
 
-    .menubar {
-        width: 100%;
-    }
+.menubar {
+    width: 100%;
+}
 
-    .p-menubar {
-        justify-content: flex-end;
-    }
+.p-menubar {
+    justify-content: flex-end;
+}
 </style>
