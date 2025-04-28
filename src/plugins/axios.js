528d2db8a2e3e312c7e6d3d64ed638bdf2750296
Carlos Rocha <carlos.rocha548@hotmail.com>
2025-04-27 21:32:22 -0300

    feat: created axios plugin

---
+++ src/plugins/axios.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { authService } from '@/services/authService';
+import router from '@/router';
+
+axios.defaults.baseURL = 'http://localhost:90/v1';
+
+axios.interceptors.request.use(
+  config => {
+    const token = authService.getToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => Promise.reject(error)
+);
+
+axios.interceptors.response.use(
+  response => {
+    if (response.data.success === false) {
+      authService.clearSession();
+      router.push('/login');
+    }
+    return response;
+  },
+  error => {
+    if (error.response && error.response.status === 401) {
+      authService.clearSession();
+      router.push('/login');
+    }
+    return Promise.reject(error);
+  }
+);
