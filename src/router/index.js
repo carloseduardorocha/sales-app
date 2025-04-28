715cc5ad1bf77992cfc6d505a3df2bd2fa18b789
Carlos Rocha <carlos.rocha548@hotmail.com>
2025-04-27 21:32:46 -0300

    feat: created auth "middleware"

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { authService } from '@/services/authService';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +9,25 @@ const router = createRouter({
       name: 'login',
       component: () => import('../views/Login.vue'),
     },
+    {
+      path: '/logout',
+      name: 'logout',
+      beforeEnter: async (to, from, next) => {
+        await authService.logout();
+        next({ name: 'login' });
+      }
+    }
   ],
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.name === 'login' && authService.isAuthenticated()) {
+    next({ name: 'sellers' });
+  } else if (to.meta.requiresAuth && !authService.isAuthenticated()) {
+    next({ name: 'login' });
+  } else {
+    next();
+  }
+});
 
 export default router
